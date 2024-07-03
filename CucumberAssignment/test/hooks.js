725e11c93dabb41f4617fd4ad0a1631001b67cd0
Ankur Tyagi <ankur.t@browserstack.com>
2024-07-03 11:03:40 +0530

    modified hooks

--- CucumberAssignment/test/hooks.js
+++ CucumberAssignment/test/hooks.js
@@ -1,13 +1,11 @@
 const { Before, After } = require('@cucumber/cucumber');
 
 Before('@setup', function () {
-  // This hook will run before scenarios tagged with @setup
   global.Hooks = 1;
   console.log(`Global variable Hooks set to: ${global.someGlobalVar}`);
 });
 
 After('@teardown', function () {
-  // This hook will run after scenarios tagged with @teardown
-  global.someGlobalVar = undefined;
+  global.Hooks = undefined;
   console.log(`Global variable Hooks cleared`);
 });
